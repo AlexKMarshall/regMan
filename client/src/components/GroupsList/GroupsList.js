6ac413ff71b789c78d7750081fc84a4e1b116cfa
Ishdril <berni2015@gmail.com>
2020-07-30 21:13:00 +0200

    fix(instrumentlist): participant total counts correctly cancelled participants

--- client/src/components/GroupsList/GroupsList.js
+++ client/src/components/GroupsList/GroupsList.js
@@ -3,18 +3,26 @@ import {InstrumentList} from '@/components';
 
 const GroupsList = ({ participants, instruments, promptPopup}) => {
   return (
-    <div>
-      Hellooo!
-      {console.log(instruments)}
-      {(instruments && participants) && instruments.map(instrument => (
-        <div key={'instrument'+instrument.id}>
-          <InstrumentList
-            participants={participants.filter(participant => participant.instrument.id === instrument.id)}
-            instrument={instrument}
-            promptPopup={promptPopup}
-          />
-        </div>
-      ))}
+    <div className="participants-list">
+      <div className="participant-grid grid-header">
+        <div className="grid-item grid-name grid-header-item">Name</div>
+        <div className="grid-item grid-status-title grid-header-item">Status</div>
+        <div className="grid-item grid-email grid-header-item">Email</div>
+        <div className="grid-item grid-underage grid-header-item">Underage?</div>
+        <div className="grid-item grid-instrument grid-header-item">Instrument</div>
+        <div className="grid-item grid-delete grid-header-item">Delete</div>
+      </div>
+      <div className="list-container">
+        {(instruments && participants) && instruments.map(instrument => (
+          <div key={'instrument'+instrument.id}>
+            <InstrumentList
+              participants={participants.filter(participant => participant.instrument.id === instrument.id)}
+              instrument={instrument}
+              promptPopup={promptPopup}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/components/InstrumentList/InstrumentList.js
+++ client/src/components/InstrumentList/InstrumentList.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { ParticipantItem } from '@/components';
 
 const InstrumentList = ({participants, instrument, promptPopup}) => {
-  const participantCounter = participants.length;
+  let participantCounter = 0;
+  const filtered = participants.filter(person => person.registration_status !== 'Cancelled');
+  participantCounter = filtered.length;
   return (
     <div>
-      {instrument.name} {participantCounter} / {instrument.max_attendants}
+      <div className="participant-grid grid-header">
+        <div className="grid-item grid-name grid-header-item">
+          {instrument.name}
+        </div>
+        <div className="grid-item grid-participants-total grid-header-item">
+          Participants: {participantCounter} / {instrument.max_attendants}
+        </div>
+      </div>
       {participants.map(attendant => (
-        <ParticipantItem participant={attendant} promptPopup={promptPopup}/>
+        <ParticipantItem key={'attendant'+attendant.id} participant={attendant} promptPopup={promptPopup}/>
       ))}
     </div>
   );
