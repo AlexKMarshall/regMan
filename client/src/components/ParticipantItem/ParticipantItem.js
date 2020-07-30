8c185d7047c525423084270040648166f12b6af2
Ishdril <berni2015@gmail.com>
2020-07-30 20:18:36 +0200

    feat(client): css for the dashboard. Added transitions to all changes for smoothnes

--- client/src/components/ParticipantItem/ParticipantItem.js
+++ client/src/components/ParticipantItem/ParticipantItem.js
@@ -4,18 +4,24 @@ import { Link } from 'react-router-dom'
 
 const ParticipantItem = ({ participant, promptPopup }) => {
   return (
-    <div className="participant">
-      <StatusLight status={participant.registration_status} />
-      <Link to={`/dashboard/details/${participant.id}/personal`}>
-        {participant.last_name}, {participant.first_name}
-      </Link>
-      <a href={`mailto:${participant.email}`} target="_blank" rel="noopener noreferrer">
-        {participant.email}
-      </a>
-      <div>{participant.is_underage ? (<span role="img" aria-label="underage">🔞</span>) : (<span role="img" aria-label="underage">✅</span>)}</div>
-      <div>{participant.instrument.name}</div>
-      <div>{participant.registration_status}</div>
-      <DeleteParticipantButton info={participant} promptPopup={promptPopup} />
+    <div className="participant-container">
+      <div className="participant-grid">
+        <Link to={`/dashboard/details/${participant.id}/personal`} className="grid-item grid-name">
+          {participant.last_name}, {participant.first_name}
+        </Link>
+        <div className="grid-item grid-status-light">
+          <StatusLight status={participant.registration_status} />
+        </div>
+        <div className="grid-item grid-status">{participant.registration_status}</div>
+        <a className="grid-item grid-email" href={`mailto:${participant.email}`} target="_blank" rel="noopener noreferrer">
+          {participant.email}
+        </a>
+        <div className="grid-item grid-underage">{participant.is_underage ? (<span role="img" aria-label="underage">🔞</span>) : (<span role="img" aria-label="underage">✅</span>)}</div>
+        <div className="grid-item grid-instrument">{participant.instrument.name}</div>
+        <div className="grid-item grid-delete">
+          <DeleteParticipantButton info={participant} promptPopup={promptPopup} />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/ParticipantList/ParticipantList.js
+++ client/src/components/ParticipantList/ParticipantList.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { ParticipantItem } from '@/components';
+import './ParticipantList.css';
 
 const ParticipantList = ({ participants, promptPopup }) => {
   return (
     <div className="participants-list">
-      {participants && participants.map(participant => (
-        <ParticipantItem
-          className="participant-container"
-          key={'participant'+participant.id}
-          participant={participant}
-          promptPopup={promptPopup}
-        />
-      ))}
+      <div className="participant-grid grid-header">
+        <div className="grid-item grid-name grid-header-item">Name</div>
+        <div className="grid-item grid-status-title grid-header-item">Status</div>
+        <div className="grid-item grid-email grid-header-item">Email</div>
+        <div className="grid-item grid-underage grid-header-item">Underage?</div>
+        <div className="grid-item grid-instrument grid-header-item">Instrument</div>
+        <div className="grid-item grid-delete grid-header-item">Delete</div>
+      </div>
+      <div className="list-container">
+        {participants ? participants.map(participant => (
+          <ParticipantItem
+            key={'participant'+participant.id}
+            participant={participant}
+            promptPopup={promptPopup}
+          />
+        )) :
+        (<h3>No one has registered yet.</h3>)
+        }
+      </div>
+
     </div>
   );
 }
