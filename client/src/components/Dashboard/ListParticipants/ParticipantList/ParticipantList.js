ef1fe83c333a4a6fe5ef79859c6512d59e917cac
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-07 20:16:20 +0100

    refactor(participantlist): remove unused useState and useEffect hooks

--- client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.js
+++ client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { ParticipantItem } from '@app/components';
 import Switch from 'react-switch';
