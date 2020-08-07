1fbce3b9a7598abd1ff3e2a23388ccdbf3710cd7
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-07 14:23:53 +0100

    test(participantlist): fix test to handle if duplicate names are in the list

--- client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.test.js
+++ client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.test.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { buildParticipant, render, screen } from '@test/test-utils';
 import ParticipantList from './ParticipantList';
-import { getByText } from '@testing-library/react';
+
+function buildParticipants() {
+  const numberOfParticipants = Math.ceil(Math.random() * 10);
+
+  let participants = [];
+  for (let i = 0; i < numberOfParticipants; i++) {
+    participants.push(buildParticipant());
+  }
+  return participants;
+}
 
 describe('ParticipantList', () => {
   test('it should render a list of participants', () => {
-    const numberOfParticipants = 5;
-
-    let participants = [];
-    for (let i = 0; i < numberOfParticipants; i++) {
-      participants.push(buildParticipant());
-    }
+    const participants = buildParticipants();
 
     render(<ParticipantList participants={participants} />);
 
     participants.forEach(({ first_name }) => {
       const nameRegex = new RegExp(first_name, 'g');
-      expect(screen.getByText(nameRegex)).toBeInTheDocument();
+      expect(screen.getAllByText(nameRegex)[0]).toBeInTheDocument();
     });
   });
 
