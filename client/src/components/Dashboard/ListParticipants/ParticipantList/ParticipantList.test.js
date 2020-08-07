b29b3a6ebee840f8aea85bdec3437459876206a9
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-07 13:59:47 +0100

    test(participantlist): test it renders a list of participants

--- client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.test.js
+++ client/src/components/Dashboard/ListParticipants/ParticipantList/ParticipantList.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@test/test-utils';
+import { buildParticipant, render, screen } from '@test/test-utils';
 import ParticipantList from './ParticipantList';
+import { getByText } from '@testing-library/react';
 
 describe('ParticipantList', () => {
-  test('it should render without error', () => {
-    render(<ParticipantList participants={[]} />);
+  test('it should render a list of participants', () => {
+    const numberOfParticipants = 5;
+
+    let participants = [];
+    for (let i = 0; i < numberOfParticipants; i++) {
+      participants.push(buildParticipant());
+    }
+
+    render(<ParticipantList participants={participants} />);
+
+    participants.forEach(({ first_name }) => {
+      const nameRegex = new RegExp(first_name, 'g');
+      expect(screen.getByText(nameRegex)).toBeInTheDocument();
+    });
   });
 });
