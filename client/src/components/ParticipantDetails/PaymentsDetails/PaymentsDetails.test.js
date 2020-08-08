7106965e42f7836ad563bb92f9cad0b93d64296d
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-08-08 20:50:51 +0200

    test(paymentdetails): test render buttons to send status and add payment

--- client/src/components/ParticipantDetails/PaymentsDetails/PaymentsDetails.test.js
+++ client/src/components/ParticipantDetails/PaymentsDetails/PaymentsDetails.test.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import PaymentsDetails from './PaymentsDetails';
+import { useAuth0 } from '@auth0/auth0-react';
+
+jest.mock('@auth0/auth0-react');
 
 describe('PaymentsDetails', () => {
-  const details = {};
+  useAuth0.mockReturnValue({
+    getAccessTokenSilently: jest.fn(() =>
+      Promise.resolve({ token: 'a fake token' })
+    ),
+  });
+
+  const details = {
+    payments: [
+      {
+        id: 1,
+        type_of_payment: 'Refund',
+        amount_paid: 5000,
+      },
+      {
+        id: 2,
+        type_of_payment: 'Refund',
+        amount_paid: 60000,
+      },
+    ],
+  };
   const setDetails = jest.fn();
 
-  it('should display date, type and amount of payment', () => {
+  it('should buttons to send status and add new payment', () => {
     render(<PaymentsDetails details={details} setDetails={setDetails} />);
+
+    expect(
+      screen.getByRole('button', { name: 'Send payment status' })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: 'Add new payment' })
+    ).toBeInTheDocument();
   });
 });
