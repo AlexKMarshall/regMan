2c877068a5cebb555db22f38af9f7b8198614370
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-09 09:05:48 +0100

    fix(dashboard): add loading state to dashboard
    Temporarily added loading state to make this testable. This will need to show a proper loading state
    eventually

--- client/src/components/Dashboard/Dashboard.test.js
+++ client/src/components/Dashboard/Dashboard.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { render } from '@test/test-utils';
+import { render, screen, waitForElementToBeRemoved } from '@test/test-utils';
 import Dashboard from './Dashboard';
 import { useAuth0 } from '@auth0/auth0-react';
+import ApiClient from '@app/services/ApiClient';
 
 jest.mock('@auth0/auth0-react');
+jest.mock('@app/services/ApiClient');
 
-test('can render dashboard with no error', () => {
+test('can render dashboard with no error', async () => {
   useAuth0.mockReturnValue({
     getAccessTokenSilently: jest.fn(() =>
       Promise.resolve({ token: 'a fake token' })
     ),
   });
 
+  ApiClient.getInstruments.mockResolvedValue([]);
+  ApiClient.getAllInscriptions.mockResolvedValue([]);
+
   render(<Dashboard />);
+  expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 });
