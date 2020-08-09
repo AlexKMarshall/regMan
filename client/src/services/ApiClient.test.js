6eaf682695db8a4889069e4ee320f13aef2dfb71
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-09 17:52:35 +0100

    test(api-client): test PutInstruments

--- client/src/services/ApiClient.test.js
+++ client/src/services/ApiClient.test.js
@@ -6,8 +6,10 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 test('getInstruments', async () => {
   const instrumentData = [buildInstrument(), buildInstrument()];
+
+  const endpoint = 'instruments';
   server.use(
-    rest.get(`${apiUrl}/instruments`, (req, res, ctx) => {
+    rest.get(`${apiUrl}/${endpoint}`, (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(instrumentData));
     })
   );
@@ -15,4 +17,23 @@ test('getInstruments', async () => {
   const returnedData = await ApiClient.getInstruments();
 
   expect(returnedData).toEqual(instrumentData);
+});
+
+test('put Instruments requires auth token to be sent', async () => {
+  const instruments = [buildInstrument(), buildInstrument()];
+  const fakeToken = 'FAKE_TOKEN';
+  const endpoint = 'instruments';
+
+  let request;
+
+  server.use(
+    rest.put(`${apiUrl}/${endpoint}`, (req, res, ctx) => {
+      request = req;
+      return res(ctx.status(200), ctx.json(instruments));
+    })
+  );
+
+  await ApiClient.putEditInstrument(instruments, fakeToken);
+
+  expect(request.headers.get('Authorization')).toBe(`Bearer ${fakeToken}`);
 });
