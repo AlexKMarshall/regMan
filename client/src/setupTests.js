d76cceb8575a58aef09c494da37762e9da99636f
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-09 13:55:08 +0100

    test(api-client): install MockServiceWorker to test api calls

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Mock Service Worker for mocking the api calls of the app
+// https://mswjs.io/docs/
+import { server } from './test/server/test-server';
+
+// Reset any request handlers that we add during tests
+// so they don't affect other tests
+afterEach(() => server.resetHandlers());
+
+// Clean up after tests finish
+afterAll(() => server.close());
---
+++ client/src/test/server/server-handlers.js
@@ -0,0 +1,12 @@
+import { rest } from 'msw';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const handlers = [
+  rest.get(`${apiUrl}/instruments`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({ instruments: [{ name: 'guitar', max_attendants: 8 }] })
+    );
+  }),
+];
