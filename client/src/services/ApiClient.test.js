5264c6ebf4453405e596f232b6c3dd6c0b4c0d00
Alex Marshall <alex.k.marshall83@gmail.com>
2020-08-12 19:36:11 +0100

    test(api-client): test more features of the client function

--- client/src/services/ApiClient.test.js
+++ client/src/services/ApiClient.test.js
@@ -16,3 +16,72 @@ test('calls fetch at the endpoint with the arguments for GET requests', async ()
 
   expect(result).toEqual(mockResult);
 });
+
+test('adds auth token when a token is provided', async () => {
+  const token = 'FAKE_TOKEN';
+
+  let request;
+  const endpoint = 'test-endpoint';
+  const mockResult = { mockValue: 'VALUE' };
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req;
+      return res(ctx.json(mockResult));
+    })
+  );
+
+  await client(endpoint, { token });
+
+  expect(request.headers.get('Authorization')).toBe(`Bearer ${token}`);
+});
+
+test('allows for config overrides', async () => {
+  let request;
+  const endpoint = 'test-endpoint';
+  const mockResult = { mockValue: 'VALUE' };
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req;
+      return res(ctx.json(mockResult));
+    })
+  );
+
+  const customConfig = {
+    mode: 'cors',
+    headers: { 'Content-Type': 'fake-type' },
+  };
+
+  await client(endpoint, customConfig);
+
+  expect(request.mode).toBe(customConfig.mode);
+  expect(request.headers.get('Content-Type')).toBe(
+    customConfig.headers['Content-Type']
+  );
+});
+
+test('when data is provided, it is stringified and the method defaults to POST', async () => {
+  const endpoint = 'test-endpoint';
+  server.use(
+    rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(req.body));
+    })
+  );
+  const data = { a: 'b' };
+  const result = await client(endpoint, { data });
+
+  expect(result).toEqual(data);
+});
+
+test(`correctly rejects the promise if there's an error`, async () => {
+  const testError = { message: 'Test error' };
+  const endpoint = 'test-endpoint';
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.status(400), ctx.json(testError));
+    })
+  );
+
+  const error = await client(endpoint).catch((e) => e);
+
+  expect(error).toEqual(testError);
+});
